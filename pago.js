0be50d298db318f906be7fec3dbbc314ba05c88a
Jesús Ochoa Rabelo <ochoarj13@hotmail.com>
2020-08-20 16:37:18 -0600

    Update pago.js

--- pago.js
+++ pago.js
@@ -12,7 +12,7 @@
 },
 "Receptor": {
 "Rfc": "AAA010101AAA",
-"Nombre": "PROMOTORES Y SUMINISTROS GH S.A. DE C.V.",
+"Nombre": "ASFALTOS DE LA FRONTERA SA DE CV",
 "ResidenciaFiscalSpecified": false,
 "NumRegIdTrib": null,
 "UsoCFDI": "P01"
@@ -43,52 +43,34 @@
 "pago10:Pagos": {
 "@xmlns:pago10": "http://www.sat.gob.mx/Pagos",
 "@Version": "1.0",
-"pago10:Pago": [
-{
+"pago10:Pago": {
 "@FechaPago": "2020-07-21T00:00:00",
 "@FormaDePagoP": "03",
 "@MonedaP": "MXN",
-"@Monto": "1507.76",
+"@Monto": "1739.02",
 "pago10:DoctoRelacionado": {
-"@Folio": "DC019334",
-"@IdDocumento": "57575997-B616-4BBF-A221-D5FCA57030A0",
-"@ImpPagado": "1507.76",
-"@ImpSaldoAnt": "1507.76",
-"@ImpSaldoInsoluto": "0",
-"@MetodoDePagoDR": "PPD",
-"@MonedaDR": "MXN",
-"@NumParcialidad": "1"
-}
-},
-{
-"@FechaPago": "2020-07-23T00:00:00",
-"@FormaDePagoP": "03",
-"@MonedaP": "MXN",
-"@Monto": "1988.61",
-"pago10:DoctoRelacionado": {
-"@Folio": "DC019387",
-"@IdDocumento": "7EADB528-D667-4003-8FDE-C28E9EDF13E7",
-"@ImpPagado": "1988.61",
-"@ImpSaldoAnt": "1988.61",
+"@Folio": "DC019238",
+"@IdDocumento": "67BE6FD9-5AD2-4019-80F1-649C12B85F84",
+"@ImpPagado": "1739.02",
+"@ImpSaldoAnt": "1739.02",
 "@ImpSaldoInsoluto": "0",
 "@MetodoDePagoDR": "PPD",
 "@MonedaDR": "MXN",
 "@NumParcialidad": "1"
 }
 }
-]
 }
 },
 {
 "tfd:TimbreFiscalDigital": {
 "@xmlns:tfd": "http://www.sat.gob.mx/TimbreFiscalDigital",
 "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-"@FechaTimbrado": "2020-08-20T13:05:42",
+"@FechaTimbrado": "2020-08-20T13:05:38",
 "@NoCertificadoSAT": "30001000000400002495",
 "@RfcProvCertif": "FCG840618N51",
-"@SelloCFD": "M46oeEWQKVjbAEBqj1mcnrg+fHg+jX3ZNaxEqe0KjwzvYGDQLsMnTyqEGRLIqa9H4SjNci3ZoohcxGN2tQQVvDbI9O4eRvxv0n9HYPGzpMzI+YqzLfNj97nRk+WFdHl5+OMjDscw18tJbPjbL40oSDj8xpPk2ceLbtxclmryRBM+VYAtmCPOrUMVUs24X/IxSEB6KuIDlLiRtLIo3MVCux2zQn7IMyv7e0Qb8y3rLBFFAtdcySirvE2bThphPhn7QkOyKJ9QRMwkt9fCLt2ZfPKCpIcIdNZjz6NYsWu85vQZi+xU+wekyNR++prk7JH+TQSphFWEO3/n8OUVRkUecA==",
-"@SelloSAT": "VShZoGLuP5bwBJY8I1MtJytNCAwDxdMa9sqVJCUYv2O/bd46whIg240ZUodpVJnweMX2skqZp8BgS6lhC1gf5NlsAsph3ky+rcnoRTXfptaWDCzQnL8goytYCorPA5diR7BDeQ+uwA9xOxlCMJZCBVxy5Ubx5U1KSWgBhHKdF/Av/Tey1GL1SmNDMUk0fjo7Fxh6VN2oNIntNwGvcXbjt9OBx4cxcSjx6dk7mKtM15y52QJpfQUvOxfCynmlACM2ZvvOKxIB8J69cWqVtJq9YZdtsOajfxQNf5n+1H45cfeRy5VUr2p/6y1YWZ0wn7vRGzT2jZKgqoTmknwGr/iZ8Q==",
-"@UUID": "FDBA81CF-E28F-4388-A1AF-2F1AF07D729D",
+"@SelloCFD": "CCdY34XU7rwns6/LVG8RN9wTWqXz3Ljo+exzcLEpo9YbL8+FmuPhoUrhAtrTDyfINyMxnVdW+3LImyg1wDQo2IbCi21vtb59erDDQcCFXfeMphhIpeWwwtHHKTZqRwPME6LFhW98JfFjWz0L0uRmVEXNY8arwuo44+50PHxWiuFNUG/A76kTV0d+jnql+BqupVGz3HSDeWurcyGmXTcMlH73iAPNq7L39I379F/9fHOwRopZehK7P/VD5IXm6hO1L9H5MY3aNE+lU7wiVA3tfKZx1ummvCaxC3v9vVC4evX/t+rm3c+JRUrb7Qv1BnKuBIEXBTilutkV8ddp9Ek8ZA==",
+"@SelloSAT": "J5Ohxq9dv7pX96IdlApzWIKiG4aefnQfMRp6dYAlXwjKGHecEgTfgOJmHj/yphvSFj60JcdmuPfY8Rgzn6VPVjwY5KD/xNCrV0j2Oj9RKxKLR/RXMQOV3oNOQySs4M7xpzfUlhUDiVzslWyxeMM1tg9zJGgPdrPIKCm6lOO6lauVFKMM3LpjNZruTg8aLHVAmLsQpOWJ6Qx0IfTf5spIHfl/vkNst4vyw//h9uTl5MwVXVe+DExgDMkw2MDWMy8bQgHVM6YvVdh/KfRu8wXr7oX0fqgws2CqmAwDVyyy8984X6g6MX9Fd80oSf0kgTqhDVJPCPE6PvlItFC0DwC9PA==",
+"@UUID": "FAA97ECE-6353-43FE-A448-1DD19F49C5C1",
 "@Version": "1.1",
 "@xsi:schemaLocation": "http://www.sat.gob.mx/TimbreFiscalDigital http://www.sat.gob.mx/sitio_internet/cfd/timbrefiscaldigital/TimbreFiscalDigitalv11.xsd"
 }
@@ -99,9 +81,9 @@
 "Addenda": null,
 "Version": "3.3",
 "Serie": null,
-"Folio": "P20080011",
-"Fecha": "2020-08-20T12:08:01",
-"Sello": "M46oeEWQKVjbAEBqj1mcnrg+fHg+jX3ZNaxEqe0KjwzvYGDQLsMnTyqEGRLIqa9H4SjNci3ZoohcxGN2tQQVvDbI9O4eRvxv0n9HYPGzpMzI+YqzLfNj97nRk+WFdHl5+OMjDscw18tJbPjbL40oSDj8xpPk2ceLbtxclmryRBM+VYAtmCPOrUMVUs24X/IxSEB6KuIDlLiRtLIo3MVCux2zQn7IMyv7e0Qb8y3rLBFFAtdcySirvE2bThphPhn7QkOyKJ9QRMwkt9fCLt2ZfPKCpIcIdNZjz6NYsWu85vQZi+xU+wekyNR++prk7JH+TQSphFWEO3/n8OUVRkUecA==",
+"Folio": "P20080009",
+"Fecha": "2020-08-20T12:07:58",
+"Sello": "CCdY34XU7rwns6/LVG8RN9wTWqXz3Ljo+exzcLEpo9YbL8+FmuPhoUrhAtrTDyfINyMxnVdW+3LImyg1wDQo2IbCi21vtb59erDDQcCFXfeMphhIpeWwwtHHKTZqRwPME6LFhW98JfFjWz0L0uRmVEXNY8arwuo44+50PHxWiuFNUG/A76kTV0d+jnql+BqupVGz3HSDeWurcyGmXTcMlH73iAPNq7L39I379F/9fHOwRopZehK7P/VD5IXm6hO1L9H5MY3aNE+lU7wiVA3tfKZx1ummvCaxC3v9vVC4evX/t+rm3c+JRUrb7Qv1BnKuBIEXBTilutkV8ddp9Ek8ZA==",
 "FormaPagoSpecified": false,
 "NoCertificado": "30001000000400002434",
 "Certificado": "MIIFuzCCA6OgAwIBAgIUMzAwMDEwMDAwMDA0MDAwMDI0MzQwDQYJKoZIhvcNAQELBQAwggErMQ8wDQYDVQQDDAZBQyBVQVQxLjAsBgNVBAoMJVNFUlZJQ0lPIERFIEFETUlOSVNUUkFDSU9OIFRSSUJVVEFSSUExGjAYBgNVBAsMEVNBVC1JRVMgQXV0aG9yaXR5MSgwJgYJKoZIhvcNAQkBFhlvc2Nhci5tYXJ0aW5lekBzYXQuZ29iLm14MR0wGwYDVQQJDBQzcmEgY2VycmFkYSBkZSBjYWRpejEOMAwGA1UEEQwFMDYzNzAxCzAJBgNVBAYTAk1YMRkwFwYDVQQIDBBDSVVEQUQgREUgTUVYSUNPMREwDwYDVQQHDAhDT1lPQUNBTjERMA8GA1UELRMIMi41LjQuNDUxJTAjBgkqhkiG9w0BCQITFnJlc3BvbnNhYmxlOiBBQ0RNQS1TQVQwHhcNMTkwNjE3MTk0NDE0WhcNMjMwNjE3MTk0NDE0WjCB4jEnMCUGA1UEAxMeRVNDVUVMQSBLRU1QRVIgVVJHQVRFIFNBIERFIENWMScwJQYDVQQpEx5FU0NVRUxBIEtFTVBFUiBVUkdBVEUgU0EgREUgQ1YxJzAlBgNVBAoTHkVTQ1VFTEEgS0VNUEVSIFVSR0FURSBTQSBERSBDVjElMCMGA1UELRMcRUtVOTAwMzE3M0M5IC8gWElRQjg5MTExNlFFNDEeMBwGA1UEBRMVIC8gWElRQjg5MTExNk1HUk1aUjA1MR4wHAYDVQQLExVFc2N1ZWxhIEtlbXBlciBVcmdhdGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCN0peKpgfOL75iYRv1fqq+oVYsLPVUR/GibYmGKc9InHFy5lYF6OTYjnIIvmkOdRobbGlCUxORX/tLsl8Ya9gm6Yo7hHnODRBIDup3GISFzB/96R9K/MzYQOcscMIoBDARaycnLvy7FlMvO7/rlVnsSARxZRO8Kz8Zkksj2zpeYpjZIya/369+oGqQk1cTRkHo59JvJ4Tfbk/3iIyf4H/Ini9nBe9cYWo0MnKob7DDt/vsdi5tA8mMtA953LapNyCZIDCRQQlUGNgDqY9/8F5mUvVgkcczsIgGdvf9vMQPSf3jjCiKj7j6ucxl1+FwJWmbvgNmiaUR/0q4m2rm78lFAgMBAAGjHTAbMAwGA1UdEwEB/wQCMAAwCwYDVR0PBAQDAgbAMA0GCSqGSIb3DQEBCwUAA4ICAQBcpj1TjT4jiinIujIdAlFzE6kRwYJCnDG08zSp4kSnShjxADGEXH2chehKMV0FY7c4njA5eDGdA/G2OCTPvF5rpeCZP5Dw504RZkYDl2suRz+wa1sNBVpbnBJEK0fQcN3IftBwsgNFdFhUtCyw3lus1SSJbPxjLHS6FcZZ51YSeIfcNXOAuTqdimusaXq15GrSrCOkM6n2jfj2sMJYM2HXaXJ6rGTEgYmhYdwxWtil6RfZB+fGQ/H9I9WLnl4KTZUS6C9+NLHh4FPDhSk19fpS2S/56aqgFoGAkXAYt9Fy5ECaPcULIfJ1DEbsXKyRdCv3JY89+0MNkOdaDnsemS2o5Gl08zI4iYtt3L40gAZ60NPh31kVLnYNsmvfNxYyKp+AeJtDHyW9w7ftM0Hoi+BuRmcAQSKFV3pk8j51la+jrRBrAUv8blbRcQ5BiZUwJzHFEKIwTsRGoRyEx96sNnB03n6GTwjIGz92SmLdNl95r9rkvp+2m4S6q1lPuXaFg7DGBrXWC8iyqeWE2iobdwIIuXPTMVqQb12m1dAkJVRO5NdHnP/MpqOvOgLqoZBNHGyBg4Gqm4sCJHCxA1c8Elfa2RQTCk0tAzllL4vOnI1GHkGJn65xokGsaU4B4D36xh7eWrfj4/pgWHmtoDAYa8wzSwo2GVCZOs+mtEgOQB91/g==",
@@ -116,4 +98,3 @@
 "LugarExpedicion": "32459",
 "Confirmacion": null
 }
-]
